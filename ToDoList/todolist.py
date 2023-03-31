3717b7e7eaaf7af8a5886ca27f502ee2ea5de870
rdyjun <rdyjun00@gmail.com>
2023-03-31 21:32:53 +0900

    Update : Disunite add page

--- ToDoList/todolist.py
+++ ToDoList/todolist.py
@@ -12,6 +12,12 @@ window.title("To-Do List")
 window.geometry("500x800")
 window.configure(bg='white')
 
+# 서브(일정 생성) 창 생성
+sub = tk.Tk()
+sub.title("To-Do List")
+sub.geometry("500x800")
+sub.configure(bg='white')
+
 
 today_date = time.localtime(time.time())
 today = time.strftime("%A, %d %B", today_date)
@@ -38,13 +44,17 @@ add_box.bind("<Button-1>", lambda event: aaa())
 add_box.configure(cursor='hand2')
 
 
+
+
 # 리스트 박스 생성
-tasks_listbox = tk.Listbox(window, width=50)
+tasks_listbox = tk.Listbox(window, width=50, height=200)
 tasks_listbox.pack(padx=10, pady=10)
+tasks_listbox.configure(bg='white', fg='#252525', highlightthickness=0, font=('Arial', 22))
 
 # 입력하는 박스 생성
-task_entry = tk.Entry(window, width=50)
+task_entry = tk.Entry(sub, width=50)
 task_entry.pack(padx=10, pady=5)
+task_entry.configure(bg='white', fg='#252525')
 
 # 일정 생성 함수
 def add_task():
@@ -54,24 +64,26 @@ def add_task():
         task_entry.delete(0, tk.END)
 
 # 일정 추가 박스 생성
-add_button = tk.Button(window, text="Add Task", command=add_task, highlightbackground=mc)
-add_button.configure(bg='white')
+add_button = tk.Button(sub, text="추가하기", command=add_task, highlightbackground='white', width=10, height=2)
+add_button.pack(padx=10, pady=5)
 
 # 일정 삭제 함수
 def remove_task():
     tasks_listbox.delete(tk.ACTIVE)
 
 # 일정 삭제 박스 생성
-remove_button = tk.Button(window, text="Remove Task", command=remove_task)
+remove_button = tk.Button(sub, text="Remove Task", command=remove_task, highlightbackground='white', width=10, height=2)
 remove_button.pack(padx=10, pady=5)
 
-# create a function to clear all tasks from the listbox
+# 전체 일정 삭제
 def clear_tasks():
     tasks_listbox.delete(0, tk.END)
 
-# create a button to clear all tasks
-clear_button = tk.Button(window, text="Clear Tasks", command=clear_tasks)
+# 전체 일정 삭제 버튼 생성
+clear_button = tk.Button(sub, text="모두 지우기", command=clear_tasks, highlightbackground='white', width=10, height=2)
 clear_button.pack(padx=10, pady=5)
 
-# run the main loop
+
+
+# window 창 열기
 window.mainloop()
