9f649eeb47185388ed4637cc9934d0abb133bfb7
rdyjun <rdyjun00@gmail.com>
2023-04-01 11:24:46 +0900

    update

--- ToDoList/todolist.py
+++ ToDoList/todolist.py
@@ -14,23 +14,24 @@ window.configure(bg='white')
 
 # 서브(일정 생성) 창 생성
 sub = tk.Tk()
-sub.title("To-Do List")
+sub.title("Create Task")
 sub.geometry("500x800")
 sub.configure(bg='white')
-
-
+#----------------------------------------------------------
+#오늘 날짜 문자열
 today_date = time.localtime(time.time())
 today = time.strftime("%A, %d %B", today_date)
-
+#----------------------------------------------------------
+# 헤더 프레임
 header_frame = tk.Frame(window, bg="white")
 header_frame.pack(padx=10, pady=10, fill=tk.X)
-
+#----------------------------------------------------------
 # 날짜 생성
 date_box = tk.Label(header_frame, text=today)
 date_box.pack(padx=(10, 50), pady=10, side=tk.LEFT)
 date_box.configure(fg = dk, bg = 'white')
 date_box.config(font=("Arial", 24, 'normal', 'bold'))
-
+#----------------------------------------------------------
 #plus 아이콘 (상대 경로)
 ps_icon = tk.PhotoImage(file="/Users/joosungjun/Documents/Dev/2023-python-class/ToDoList/img/plus.png")
 
@@ -43,19 +44,17 @@ add_box.pack(side=tk.RIGHT, padx=(10, 20), pady=10)
 add_box.bind("<Button-1>", lambda event: aaa())
 add_box.configure(cursor='hand2')
 
-
-
-
-# 리스트 박스 생성
-tasks_listbox = tk.Listbox(window, width=50, height=200)
+#----------------------------------------------------------
+# 일정 리스트 박스 생성
+tasks_listbox = tk.Listbox(window, width=50, height=20)
 tasks_listbox.pack(padx=10, pady=10)
 tasks_listbox.configure(bg='white', fg='#252525', highlightthickness=0, font=('Arial', 22))
-
+#----------------------------------------------------------
 # 입력하는 박스 생성
-task_entry = tk.Entry(sub, width=50)
+task_entry = tk.Entry(sub, width=50, height=3, highlightbackground='white', highlightthickness=0)
 task_entry.pack(padx=10, pady=5)
-task_entry.configure(bg='white', fg='#252525')
-
+task_entry.configure(bg='white', fg='#252525', font=('Arial', 20))
+#----------------------------------------------------------
 # 일정 생성 함수
 def add_task():
     task = task_entry.get()
@@ -64,25 +63,26 @@ def add_task():
         task_entry.delete(0, tk.END)
 
 # 일정 추가 박스 생성
-add_button = tk.Button(sub, text="추가하기", command=add_task, highlightbackground='white', width=10, height=2)
-add_button.pack(padx=10, pady=5)
-
+add_button = tk.Button(sub, text="추가하기", command=add_task, highlightbackground='white', width=100, height=3)
+add_button.pack(side='bottom')
+add_button.configure(fg=dk, bg=mc, font = ('Arial', 20, 'bold'))
+#----------------------------------------------------------
 # 일정 삭제 함수
 def remove_task():
     tasks_listbox.delete(tk.ACTIVE)
-
+    
 # 일정 삭제 박스 생성
-remove_button = tk.Button(sub, text="Remove Task", command=remove_task, highlightbackground='white', width=10, height=2)
+remove_button = tk.Button(window, text="Remove Task", command=remove_task, highlightbackground='white', width=10, height=2)
 remove_button.pack(padx=10, pady=5)
-
+#----------------------------------------------------------
 # 전체 일정 삭제
 def clear_tasks():
     tasks_listbox.delete(0, tk.END)
 
 # 전체 일정 삭제 버튼 생성
-clear_button = tk.Button(sub, text="모두 지우기", command=clear_tasks, highlightbackground='white', width=10, height=2)
+clear_button = tk.Button(window, text="모두 지우기", command=clear_tasks, highlightbackground='white', width=10, height=2)
 clear_button.pack(padx=10, pady=5)
-
+#----------------------------------------------------------
 
 
 # window 창 열기
